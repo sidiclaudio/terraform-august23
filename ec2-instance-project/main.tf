8b347678501668557b190dfc46814b7b8fe1e61a
Claudio <sidiclaudio90@gmail.com>
2024-04-14 09:40:32 -0700

    add user data

--- ec2-instance-project/main.tf
+++ ec2-instance-project/main.tf
@@ -1,7 +1,6 @@
 # This is main code
 
 data "aws_subnet" "public_subnet" {
-  #id = "subnet-0547bfd22735d8418" # lookup the Subnet ID value
   filter {
     name   = "tag:Name"
     values = ["awesome-class-pub-sub1"]
@@ -14,8 +13,47 @@ resource "aws_instance" "decaf_coffee_instance" {
   key_name                = var.key_name
   subnet_id               = data.aws_subnet.public_subnet.id # Reference the data source 
   disable_api_termination = var.enable_termination_protection
+  vpc_security_group_ids  = [aws_security_group.web_ec2_sg.id] # Attach the SG ID from resource below
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<html><body><h1>EC2 deployed with Terraform love!!!</h1></body></html>" > /var/www/html/index.html
+              EOF             
 
   tags = {
     Name = "decaf_coffee_ec2_instance"
+  }
+}
+
+resource "aws_security_group" "web_ec2_sg" {
+  name        = "allow-web-traffic-sg-terraform"
+  description = "Allow WEB inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_subnet.public_subnet.vpc_id # laziness!!!
+
+  ingress  {
+    description = "allow ssh traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # dont do this
+  }
+
+  ingress  {
+    description = "allow web traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
--- ec2-instance-project/outputs.tf
+++ ec2-instance-project/outputs.tf
@@ -1,1 +1,15 @@
 # Setup Output values
+output "public_ip" {
+  description = "return the public ip of the EC2 instance"
+  value = aws_instance.decaf_coffee_instance.public_ip
+}
+
+output "dns_name" {
+  description = "return Public DNS name assigned to the instance"
+  value = aws_instance.decaf_coffee_instance.public_dns
+}
+
+output "instance_id" {
+  description = "return ID of the instance."
+  value = aws_instance.decaf_coffee_instance.id
+}
